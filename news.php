147c22bfe4b006cdb4a5089b3b49a158ed989302
Carver Harrison <pcarverb@hotmail.com>
2016-08-05 14:57:05 -0700

    Create news.php

---
+++ news.php
@@ -0,0 +1,39 @@
+<html>
+  <head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="main.css">
+    <title>ShiftOS - News</title>
+  </head>
+  <body>
+    <div class="container">
+      <header style="display:inline">
+        <img
+          src="res/logo-wide.png"
+          alt="ShiftOS Logo"
+          style="float:left"
+        />
+        
+        <h1 style="float:right">News</h1>
+      </header>
+      <main>
+        <section class="jumbotron">
+          <h2>ShiftOS Laucher 2.0 Beta 1</h2>
+          <div class="post">
+            <div class="post-info">
+              <p>First Beta of The ShiftOS Launcher 2.0</p>
+            </div>
+            <div class="post-features">
+            <ul>
+              <li>New UI</li>
+              <li>New UX</li>
+              <li>New Server</li>
+            </ul>
+          </div>
+          <div class="post-author">
+            <p class="post-author-p">By <span class="post-author-span">Carver</span></p>
+          </div>
+        </section>
+      </main>
+    </div>
+  </body>
+</html>
