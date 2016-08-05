024bed3a856b30e7cc7f62d79dc8ba4469eb33e7
Carver Harrison <pcarverb@hotmail.com>
2016-08-05 15:01:11 -0700

    Update index.php

--- index.php
+++ index.php
@@ -4,12 +4,19 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   </head>
   <body>
-    <h1>Welcome to the ShiftOS Update Server!</h1>
-    <ul>
-      <li>
-        <a href="/">Home</a>
-        <a href="/news.php">News</a>
-        <a href="/releases">Releases</a>
-    </ul>
+    <main class="container">
+      <h1>Welcome to the ShiftOS Update Server!</h1>
+      <ul>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/news.php">News</a>
+        </li>
+        <li>
+          <a href="/releases">Releases</a>
+        </li>
+      </ul>
+    </main>
   </body>
 </html>
