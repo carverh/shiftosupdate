3170cd54c90937ef5ab11e721eb21f8cf4598116
Carver Harrison <pcarverb@hotmail.com>
2016-08-05 14:59:47 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+  <head>
+    <title>ShiftOS Update Server</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  </head>
+  <body>
+    <h1>Welcome to the ShiftOS Update Server!</h1>
+    <ul>
+      <li>
+        <a href="/">Home</a>
+        <a href="/news.php">News</a>
+        <a href="/releases">Releases</a>
+    </ul>
+  </body>
+</html>
